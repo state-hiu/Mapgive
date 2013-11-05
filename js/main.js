f86675a73cb03a6c34e1e03a49f6c232e54904bc
Nathan Kleekamp <nkleekamp@gmail.com>
2013-11-05 11:05:56 -0500

    basic site laid out

---
+++ js/main.js
@@ -0,0 +1,55 @@
+(function($, window, document, undefined) {
+  var $panels = $('#accordion .panel-collapse');
+  var $panelTitles = $('#accordion .panel-title');
+
+  // Collapse accordions at page load and set appropriate default aria attributes
+  // in case Javascript is turned off
+  function initAccordion() {
+    $panels.removeClass('in').addClass('collapse').attr('aria-hidden', 'true');
+    $panelTitles.attr('aria-expanded', 'false');
+  }
+
+  // Set appropriate aria attributes on accordion when changed
+  function setAriaAttr() {
+    var $this = $(this);
+    var $expanded = $this.attr('aria-expanded');
+    var $target = $('#' + $this.attr('aria-controls'));
+
+    if ($expanded == 'false') {
+      $this.attr({
+        'aria-expanded': 'true',
+        'aria-selected': 'true'
+      });
+      $target.attr('aria-hidden', 'false');
+    } else {
+      $this.attr({
+        'aria-expanded': 'false',
+        'aria-selected': 'false'
+      });
+      $target.attr('aria-hidden', 'true');
+    };
+  }
+
+  // Activate OSM tabs on Instructions page
+  $('#osmTab a').click(function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+  });
+
+  // Activate HOT tabs on Instructions page
+  $('#hotTab a').click(function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+  })
+
+  // Init the accordion
+  initAccordion();
+
+  $panelTitles.on('click', setAriaAttr);
+  $panelTitles.on('keydown', function(e) {
+    if (e.which == 13) {
+      setAriaAttr();
+    };
+  });
+
+})(jQuery, window, document);
